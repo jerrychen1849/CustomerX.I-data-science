44f8a7e1f949f0fef8086ced54a183a61036d6c7
jerrychen1849 <60303661+jerrychen1849@users.noreply.github.com>
2023-02-22 20:16:56 -0800

    Add files via upload
    Google API to return restaurant info when entering restaurant ID

---
+++ restaurant_info.py
@@ -0,0 +1,33 @@
+import pip._vendor.requests
+import json
+
+# Put your API key here
+api_key = 'AIzaSyB8rzDHNa0MhBaBN-VKq7baPe_fox0h95c'
+
+# Function to get restaurant information from Google Maps Places API
+def get_restaurant_info(place_id):
+    url = f'https://maps.googleapis.com/maps/api/place/details/json?place_id={place_id}&fields=name,types,permanently_closed&key={api_key}'
+    response = pip._vendor.requests.get(url)
+    restaurant_info = json.loads(response.text)
+    return restaurant_info
+
+# Function to get the types of food a restaurant serves
+def get_food_types(place_id):
+    restaurant_info = get_restaurant_info(place_id)
+    if 'types' in restaurant_info['result']:
+        food_types = [type for type in restaurant_info['result']['types']]
+        return food_types
+    else:
+        return None
+
+# Example usage
+place_id = 'ChIJEy05P-fHwoAR1eHcd5k0UWs'
+food_types = get_food_types(place_id)
+print(food_types)
+
+if food_types == None:
+    print("No information about food types or restaurant is closed permanently")
+else:
+    print(food_types)
+
+#Enable the Google Places API on Google Cloud Console.
